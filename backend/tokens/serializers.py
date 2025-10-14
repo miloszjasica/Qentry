a6c5848bb9d66156d8c796382844e404137e57fc
Milosz <miloszjasica1@gmail.com>
2025-10-14 14:52:07 +0200

    Added(Transactions endpoints)

--- backend/tokens/serializers.py
+++ backend/tokens/serializers.py
@@ -1,15 +1,13 @@
 from rest_framework import serializers
-from .models import QR, Transaction
-
-class QRSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QR
-        fields = ['id', 'code', 'created_at', 'is_used', 'event', 'attraction', 'user']
+from .models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='id_user.name', read_only=True)
+    attraction_name = serializers.CharField(source='id_attraction.name', read_only=True)
+    price = serializers.DecimalField(source='id_attraction.price', max_digits=10, decimal_places=2, read_only=True)
+    event_name = serializers.CharField(source='id_attraction.id_event.name', read_only=True)
+    date = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
     class Meta:
         model = Transaction
-        fields = ['id', 'user', 'attraction', 'date']
-
-class PurchaseTokensSerializer(serializers.Serializer):
-    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
+        fields = ['id_transaction', 'user_name', 'event_name', 'attraction_name', 'price', 'date']
--- backend/tokens/urls.py
+++ backend/tokens/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/<int:user_id>/<int:id_event>', views.get_tokens, name='get_tokens'),
-    path('users/<int:user_id>/<int:id_event>/add', views.purchase_tokens, name='purchase-tokens'),
+    path('users/<int:user_id>/<int:id_event>', views.user_balance, name='user-balance'),
+    path('users/<int:user_id>/<int:id_event>/add', views.add_tokens, name='add-tokens'),
+    path('transactions/<int:user_id>/<int:id_event>/<int:id_attraction>', views.new_transaction, name='new-transaction'),
+    path('transactions/<int:transaction_id>', views.get_transaction, name='transaction-details'),
+    path('transactions/', views.list_transactions, name='list-transactions'),
+    path('users/<int:user_id>/transactions', views.user_transactions, name='user-transactions'),
+    path('attractions/<int:id_attraction>/transactions', views.attraction_transactions, name='history-attraction-transactions'),
 ]
