11a791da086ba4d585896def99ffcf04079c2b68
Patryk <patrykkoksjarosinski@gmail.com>
2025-10-14 22:06:59 +0200

    added( GET/events, POST events/create)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path('api/tokens/', include('tokens.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    path('events/', include('events.urls')),
 ]
