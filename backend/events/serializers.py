46217b3897e4f6c156f545f3048271b01c4bbcbb
Patryk <patrykkoksjarosinski@gmail.com>
2025-11-06 20:10:43 +0100

    zabezpieczenia

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -5,8 +5,14 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+        read_only_fields = ['user_id']
 
     def validate(self, data):
         if data['start_date'] > data['end_date']:
             raise serializers.ValidationError("Start date cannot be later than end date.")
         return data
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        validated_data['user_id'] = user
+        return super().create(validated_data)
