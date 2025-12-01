4fa04e33d590257938b08c7889f48c86424951db
Milosz <miloszjasica1@gmail.com>
2025-12-01 18:47:33 +0100

    view(take part in event + fixes)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,32 @@ import Register from "./pages/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import NearbyEvents from "./pages/NearbyEvents";
-import { useState } from "react";
+import TakePartEvents from "./pages/TakePartEvents";
+import { useState, useEffect } from "react";
 
 function App() {
 
   const [search, setSearch] = useState("");
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
+  const token = localStorage.getItem("token");
+
+
+useEffect(() => {
+  const token = localStorage.getItem("token");
+  if (!token) return;
+
+  fetch("/api/users/me/", {
+    headers: { "Authorization": `Bearer ${token}` },
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log("User fetched:", data);
+      setCurrentUser(data);
+    })
+    .catch(err => console.error(err));
+}, []);
+
 
   return (
     <Router>
@@ -28,12 +48,13 @@ function App() {
         </div>
       </div>
         <Navbar isExpanded={isSidebarExpanded} setIsExpanded={setIsSidebarExpanded}/>
-        <Header search={search} setSearch={setSearch} isExpanded={isSidebarExpanded} />
-        <div style={{ flex: 1, marginLeft: isSidebarExpanded ? 256 : 80, transition: "margin-left 0.3s", marginTop: "72px", overflow: "visible" }}>
+        <Header search={search} setSearch={setSearch} isExpanded={isSidebarExpanded} user={currentUser}/>
+        <div style={{ flex: 1, marginLeft: isSidebarExpanded ? 256 : 80, transition: "margin-left 0.3s", marginTop: "0px", overflow: "visible" }}>
           <Routes>
             <Route path="/" element={<Home search={search} />} />
             {/* <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} /> */}
+            <Route path="/biore-udzial" element={<TakePartEvents search={search} />} />
             <Route path="/w-poblizu" element={<NearbyEvents search={search} />} />
           </Routes>
         </div>
