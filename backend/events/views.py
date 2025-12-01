987d138834e4fd01cb225592156dc0ef39e3f0b8
Milosz <miloszjasica1@gmail.com>
2025-12-01 13:40:57 +0100

    endpoint events of organizer

--- backend/events/views.py
+++ backend/events/views.py
@@ -90,6 +90,82 @@ def get_events(request):
     serializer = EventSerializer(queryset, many=True)
     return Response(serializer.data)
 
+@extend_schema(
+    tags=['Events'],
+    summary='Get list of organizer events',
+    description='Zwraca listę eventów stworzonych przez organizatora z możliwością filtrowania po niektórych polach z modelu event.',
+    parameters=[
+        OpenApiParameter(
+            name='name',
+            description='Filtr po nazwie wydarzenia (wyszukiwanie częściowe)',
+            required=False,
+            type=str
+        ),
+        OpenApiParameter(
+            name='location',
+            description='Filtr po lokalizacji (wyszukiwanie częściowe)',
+            required=False,
+            type=str
+        ),
+        OpenApiParameter(
+            name='category',
+            description='Filtr po kategorii wydarzenia',
+            required=False,
+            type=str,
+            enum=[
+                "music", "art", "food", "sport", "business", "theatre",
+                "tech", "wellness", "gaming", "film", "fashion", "books", "other"
+            ]
+        ),
+        OpenApiParameter(
+            name='date',
+            description='Filtr po dacie (eventy aktywne w danym dniu, format YYYY-MM-DD)',
+            required=False,
+            type=str,
+        ),
+        OpenApiParameter(
+            name='is_active',
+            description='Filtr po statusie aktywności (true/false)',
+            required=False,
+            type=bool
+        ),
+    ],
+    responses={200: OpenApiResponse(response=EventSerializer, description='Lista eventów')},
+)
+@api_view(['GET'])
+def get_organizer_events(request):
+    try:
+        queryset = Event.objects.filter(user_id=request.user.id)
+        user = request.user
+
+        name = request.query_params.get('name')
+        location = request.query_params.get('location')
+        is_active = request.query_params.get('is_active')
+        category = request.query_params.get('category')
+        date = request.query_params.get('date')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        if location:
+            queryset = queryset.filter(location__icontains=location)
+
+        if is_active is not None:
+            queryset = queryset.filter(is_active=is_active.lower() == "true")
+
+        if category:
+            queryset = queryset.filter(category=category)
+
+        if date:
+            queryset = queryset.filter(start_date__lte=date, end_date__gte=date)
+
+        if user.role != ['organizer', 'admin']:
+            serializer = EventSerializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            return Response({'error': 'You have to be an organizer'}, status=status.HTTP_403_FORBIDDEN)
+    except Exception as e:
+        return Response({'error'}, status=status.HTTP_400_BAD_REQUEST)
 
 @extend_schema(
     tags=['Events'],
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,12 @@ import NearbyEvents from "./pages/NearbyEvents";
 import { useState } from "react";
 
 function App() {
+
   const [search, setSearch] = useState("");
   const [isSidebarExpanded, setIsSidebarExpanded] = useState(true);
 
   return (
     <Router>
-      {/* Usuń outer div style={{ display: "flex" }} jeśli ma sens tylko dla layoutu Home */}
-      <Header search={search} setSearch={setSearch} />
-      <Navbar />
-
-      {/* Dynamiczny Kontent */}
-      <div style={{ flex: 1, marginLeft: "256px", marginTop: "72px" }}>
       <div style={{ display: "flex" }}>
 
         <div style={{ flex: 1, marginLeft: isSidebarExpanded ? 256 : 80, transition: "margin-left 0.3s",  marginTop: "72px" }}>
@@ -26,16 +21,12 @@ function App() {
 
             {/* <Route path="/" element={<Home search={search} />} /> */}
 
-        <Routes>
-          {/* Użyj elementu Home z propsem 'search' tylko na ścieżce głównej */}
-          <Route path="/" element={<Home search={search} />} />
+            <Route path="/login" element={<Login />} />
 
-          {/* Na tych ścieżkach renderuj tylko komponent Login/Register */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
-
         <Navbar isExpanded={isSidebarExpanded} setIsExpanded={setIsSidebarExpanded}/>
         <Header search={search} setSearch={setSearch} isExpanded={isSidebarExpanded} />
         <div style={{ flex: 1, marginLeft: isSidebarExpanded ? 256 : 80, transition: "margin-left 0.3s", marginTop: "72px", overflow: "visible" }}>
