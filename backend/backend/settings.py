2cd3a89ac13970472190900f3cb7dbec744efa25
Milosz <miloszjasica1@gmail.com>
2026-01-16 16:40:48 +0100

    (staticfiles)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$c2qk4j9i1jz#rs*@1*(2*t&6+td9*-@)y45js&*mj4j=6no)k'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2','192.168.201.1', '57.128.249.150']
 
@@ -134,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
